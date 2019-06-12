e55443209bd1cfb68a4fe80efcacf35ec4905680
fanzheng <montage_fz@sina.com>
2019-06-12 14:01:15 +0800

    - 使用postman发送post请求

--- 03. 原生接口开发/02. node处理POST请求.js
+++ 03. 原生接口开发/02. node处理POST请求.js
@@ -1,3 +1,28 @@
 /**
  * Created by MonTage_fz on 2019/6/12
  */
+const http = require('http');
+const server = http.createServer((req, res) => {
+    const requestMethod = req.method;
+    if (requestMethod === 'POST') {
+        // 数据格式
+        console.log(`content-type:${req.headers['content-type']}`);
+        // 接收数据
+        let postData = '';
+        req.on('data', chunk => {
+            // chunk 是二进制
+            postData += chunk.toString();
+        });
+        req.on('end', () => {
+            console.log(postData);
+            res.end('method is post');
+        });
+        
+    } else {
+        res.end('method is get');
+    }
+    
+});
+server.listen(3000, () => {
+    console.log(`connect 3000 port!`);
+});
