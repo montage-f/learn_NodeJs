d0383a0aa9e13ddfd819457ee84741d2fa1b59c5
fanzheng <montage_fz@sina.com>
2019-06-12 14:50:35 +0800

    - 综合使用post和get请求

---
+++ 03. 原生接口开发/03. 处理http请求的综合示例.js
@@ -0,0 +1,42 @@
+/**
+ * Created by MonTage_fz on 2019/6/12
+ */
+const http = require('http');
+const queryString = require('querystring');
+const server = http.createServer((req, res) => {
+    // 告诉前端返回的数据类型是json格式
+    res.setHeader('Content-type', 'application/json');
+    
+    const url = req.url;
+    const method = req.method;
+    const path = url.split('?')[0];
+    const query = queryString.parse(url.split('?')[1]);
+    
+    let data = {
+        url,
+        method,
+        path,
+        query,
+    };
+    if (method === 'GET') {
+        // 这里只允许是字符串, 但是字符串的格式是json
+        console.log('get', data);
+        res.end(JSON.stringify(data));
+    } else if (method === 'POST') {
+        let postData = '';
+        req.on('data', (chunk) => {
+            postData += chunk.toString();
+        });
+        req.on('end', () => {
+            data = {
+                ...data,
+                postData
+            };
+            console.log('post', data);
+            res.end(JSON.stringify(data));
+        });
+    }
+});
+server.listen(3000, () => {
+    console.log(`connect 3000 port!`);
+});
