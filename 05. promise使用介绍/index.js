7f1fc6d484a5e64f5512590ac8de9a15b70b3643
fanzheng <montage_fz@sina.com>
2019-06-24 13:26:00 +0800

    - 设置session

---
+++ 05. promise使用介绍/index.js
@@ -0,0 +1,57 @@
+/**
+ * Created by MonTage_fz on 2019/6/12
+ */
+const fs = require('fs');
+const path = require('path');
+const fullFileName = path.resolve(__dirname, 'files', 'a.json');
+console.log('fullFileName', fullFileName);
+fs.readFile(fullFileName, (err, data) => {
+    if (err) {
+        console.log(`err`, err);
+        return;
+    }
+    console.log('data', data.toString());
+});
+
+const readFile = (fileName, callback) => {
+    const filePath = path.resolve(__dirname, 'files', fileName);
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            console.log('err', err);
+            return;
+        }
+        callback(JSON.parse(data));
+    });
+};
+// 回调处理异步
+readFile('a.json', (a) => {
+    console.log('a', a);
+    readFile(a.next, (b) => {
+        console.log('b', b);
+        readFile(b.next, (c) => {
+            console.log('c', c);
+        });
+    });
+});
+
+
+// promise 处理异步
+const promiseReadFile = (fileName) => new Promise((resolve, reject) => {
+    const filePath = path.resolve(__dirname, 'files', fileName);
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            reject(err);
+        }
+        resolve(JSON.parse(data));
+        
+    });
+});
+promiseReadFile('a.json').then((data) => {
+    console.log('Pa', data);
+    return promiseReadFile(data.next);
+}).then((data) => {
+    console.log('Pb', data);
+    return promiseReadFile(data.next);
+}).then((data) => {
+    console.log('Pc', data);
+});
