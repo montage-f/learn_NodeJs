48426a1ca25f25ea298d58f4e496c9b30377e84f
fanzheng <montage_fz@sina.com>
2019-07-03 18:39:31 +0800

    - [修改] 添加备注

--- 08. express-server/app.js
+++ 08. express-server/app.js
@@ -1,8 +1,8 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const cookieParser = require('cookie-parser'); // 用来解析cookie
+const logger = require('morgan'); // 生成日志, 写日志
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -14,11 +14,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.json()); // 解析 post 请求
+app.use(express.urlencoded({extended: false})); // 解析 post 的其他类型数据
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'))); // 将静态文件返回
 
+// 给路由设置跟路径
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
@@ -31,7 +32,7 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.locals.error = req.app.get('env') === 'dev' ? err : {};
     
     // render the error page
     res.status(err.status || 500);
