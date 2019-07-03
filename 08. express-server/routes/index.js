bb137604d061d5586c157a618f70831db33b2efe
fanzheng <montage_fz@sina.com>
2019-07-03 17:45:33 +0800

    - [修改] 格式化文件

--- 08. express-server/routes/index.js
+++ 08. express-server/routes/index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  console.log(22);
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    console.log(22);
+    res.render('index', {title: 'Express'});
 });
 
 module.exports = router;
